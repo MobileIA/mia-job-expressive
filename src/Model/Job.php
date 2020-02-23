e87612331d623452ecaf3584ff14508cb6c57984
Matias Camiletti <matias.camiletti@gmail.com>
2020-02-23 17:07:41 -0300

    Agregado modelo de DB.

---
+++ src/Model/Job.php
@@ -0,0 +1,17 @@
+<?php namespace Mobileia\Expressive\Job\Model;
+
+/**
+ * Description of Job
+ *
+ * @author matiascamiletti
+ */
+class Job extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'jobs';
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    //public $timestamps = false;
+}
